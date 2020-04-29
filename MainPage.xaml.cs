365993409e446f802805158af6682276eaa02417
Matt Arnold <mattarnster.co.uk@gmail.com>
2020-04-29 00:20:40 -0400

    Initial stuff

---
+++ MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Reflection;
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RadarrApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            if (roamingSettings.Values["serverURL"] == null || (string) roamingSettings.Values["serverURL"] == "")
+            {
+                NavigateToView("FirstRun", NavView);
+            }
+            else
+            {
+                NavigateToView("Movies", NavView);
+            }
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                NavigateToView("Settings", sender);
+            } 
+            else
+            {
+                NavigationViewItem item = (NavigationViewItem) args.SelectedItem;
+                NavigateToView((string)item.Content, sender);
+            }
+        }
+
+
+        private bool NavigateToView(string clickedView, NavigationView nav)
+        {
+            var view = Assembly.GetExecutingAssembly()
+                .GetType($"RadarrApp.Views.{clickedView}");
+
+            if (string.IsNullOrWhiteSpace(clickedView) || view == null)
+            {
+                return false;
+            }
+
+            contentFrame.Navigate(view, null);
+
+            return true;
+        }
+
+        private void NavView_BackRequested(NavigationView sender,
+                                   NavigationViewBackRequestedEventArgs args)
+        {
+            On_BackRequested();
+        }
+
+        private bool On_BackRequested()
+        {
+            if (!contentFrame.CanGoBack)
+                return false;
+
+            // Don't go back if the nav pane is overlayed.
+            if (NavView.IsPaneOpen &&
+                (NavView.DisplayMode == NavigationViewDisplayMode.Compact ||
+                 NavView.DisplayMode == NavigationViewDisplayMode.Minimal))
+                return false;
+
+            contentFrame.GoBack();
+            return true;
+        }
+
+        public void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            NavView.IsBackEnabled = contentFrame.CanGoBack;
+        }
+
+
+    }
+}
---
+++ Views/Movies.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RadarrApp.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Movies : Page
+    {
+        ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+        RadarrSharp.RadarrClient radarrClient;
+        private IList<RadarrSharp.Models.Movie> movies;
+
+        public Movies()
+        {
+            this.InitializeComponent();
+            radarrClient = new RadarrSharp.RadarrClient((string)roamingSettings.Values["serverURL"], 7878, (string)roamingSettings.Values["apiKey"]);
+
+            GetMovies();
+        }
+
+        private async void GetMovies()
+        {
+            movies = await radarrClient.Movie.GetMovies();
+            foreach (var movie in movies)
+            {
+                foreach (var image in movie.Images)
+                {
+                    image.Url = "http://" + radarrClient.Host + ":" + radarrClient.Port + "/api" + image.Url + "?apikey=" + radarrClient.ApiKey;
+                }
+            }
+            progressRing.IsActive = false;
+            ContentGridView.ItemsSource = movies;
+        }
+
+        private void ContentGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            RadarrSharp.Models.Movie clickedMovie = (RadarrSharp.Models.Movie)e.ClickedItem;
+            Frame.Navigate(typeof(RadarrApp.Views.Movie), clickedMovie, new EntranceNavigationTransitionInfo());
+        }
+    }
+}
